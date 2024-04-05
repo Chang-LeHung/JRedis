f8324a9903ca368422914ba585d76106b6eafbc5
changlehung <changlehung@gmail.com>
2024-04-05 13:08:35 +0800

    add the JRedis serializer

---
+++ src/main/java/org/jredis/JRSerializerUtil.java
@@ -0,0 +1,41 @@
+package org.jredis;
+
+import org.jredis.exception.JRedisTypeNotMatch;
+import org.jredis.hash.JRIncrementalHash;
+import org.jredis.list.JRList;
+import org.jredis.string.JRString;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JRSerializerUtil {
+
+  public static JRedisObject deserialize(byte[] data, int low, int high) throws JRedisTypeNotMatch, IOException {
+    ByteArrayInputStream stream = new ByteArrayInputStream(data, low, high - low);
+    return deserialize(stream);
+  }
+
+  public static JRedisObject deserialize(InputStream stream) throws JRedisTypeNotMatch, IOException {
+    stream.mark(1);
+    int idx = stream.read();
+    stream.reset();
+    JRedisObject o = null;
+    switch (JRType.VALUES[idx]) {
+      case LIST -> {
+        o = new JRList();
+        o.deserialize(stream);
+      }
+      case SET -> {}
+      case STRING -> {
+        o = new JRString();
+        o.deserialize(stream);
+      }
+      case HASH -> {
+        o = new JRIncrementalHash<>();
+        o.deserialize(stream);
+      }
+    }
+    return o;
+  }
+}
