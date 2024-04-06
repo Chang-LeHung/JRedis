e6bdfa76404e93a50182f929184fac05a65c36f8
changlehung <changlehung@gmail.com>
2024-04-06 12:45:09 +0800

    add JRedis Number objects

---
+++ src/main/java/org/jredis/number/JRDouble.java
@@ -0,0 +1,85 @@
+package org.jredis.number;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.apache.commons.io.EndianUtils;
+import org.jredis.JRType;
+import org.jredis.JRedisObject;
+import org.jredis.exception.JRedisTypeNotMatch;
+
+public class JRDouble extends JRedisObject {
+
+  private double val;
+
+  public JRDouble(double val) {
+    this.val = val;
+  }
+
+  public int serialSize() {
+    return 9;
+  }
+
+  @Override
+  public int serialize(OutputStream out) throws IOException {
+    out.write(JRType.DOUBLE.FLAG_NUMBER);
+    EndianUtils.writeSwappedDouble(out, val);
+    return 9;
+  }
+
+  @Override
+  public int deserialize(InputStream stream) throws IOException, JRedisTypeNotMatch {
+    if (stream.read() != JRType.DOUBLE.FLAG_NUMBER)
+      throw new JRedisTypeNotMatch("require a double object");
+    val = EndianUtils.readSwappedDouble(stream);
+    return 9;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof JRDouble other) {
+      return val == other.val;
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "JRDouble{" +
+        "val=" + val +
+        '}';
+  }
+
+  public double getVal() {
+    return val;
+  }
+
+  public void setVal(double val) {
+    this.val = val;
+  }
+
+  public void increment(double val) {
+    this.val += val;
+  }
+
+  public void decrement(double val) {
+    this.val -= val;
+  }
+
+  public void multiply(double val) {
+    this.val *= val;
+  }
+
+  public void divide(double val) {
+    this.val /= val;
+  }
+
+  public void mod(double val) {
+    this.val %= val;
+  }
+
+  @Override
+  public int hashCode() {
+    return Double.hashCode(val);
+  }
+}
---
+++ src/main/java/org/jredis/number/JRInt.java
@@ -0,0 +1,87 @@
+package org.jredis.number;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.apache.commons.io.EndianUtils;
+import org.jredis.JRType;
+import org.jredis.JRedisObject;
+import org.jredis.exception.JRedisTypeNotMatch;
+
+public class JRInt extends JRedisObject {
+
+  private long val;
+
+  public JRInt(long val) {
+    this.val = val;
+  }
+
+  public long getVal() {
+    return val;
+  }
+
+  public void setVal(long val) {
+    this.val = val;
+  }
+
+  @Override
+  public int serialize(OutputStream out) throws IOException {
+    out.write(JRType.INT.FLAG_NUMBER);
+    EndianUtils.writeSwappedLong(out, val);
+    return 9;
+  }
+
+  @Override
+  public int deserialize(InputStream stream) throws IOException, JRedisTypeNotMatch {
+    if (stream.read() != JRType.INT.FLAG_NUMBER) {
+      throw new JRedisTypeNotMatch("Not a JRInt");
+    }
+    val = EndianUtils.readSwappedLong(stream);
+    return 9;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (null == obj) return false;
+    if (obj instanceof JRInt jrInt) {
+      return jrInt.val == val;
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "JRInt{" +
+        "val=" + val +
+        '}';
+  }
+
+  public int serialSize() {
+    return 9;
+  }
+
+  public void increment(long val) {
+    this.val += val;
+  }
+
+  public void decrement(long val) {
+    this.val -= val;
+  }
+
+  public void mod(long val) {
+    this.val %= val;
+  }
+
+  public void multiply(long val) {
+    this.val *= val;
+  }
+
+  public void divide(long val) {
+    this.val /= val;
+  }
+
+  @Override
+  public int hashCode() {
+    return Long.hashCode(val);
+  }
+}
