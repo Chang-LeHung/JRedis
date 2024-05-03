9dfdba610d58c09fe3f1a5bb8697fc599d264aa6
changlehung <changlehung@gmail.com>
2024-05-03 16:37:21 +0800

    test: inspect object memory layout

---
+++ src/test/java/org/memorylayout/JRedisServerTest.java
@@ -0,0 +1,42 @@
+package org.memorylayout;
+
+import org.jredis.RedisDatabase;
+import org.jredis.hash.JRIncrementalHash;
+import org.jredis.server.JRedisConfiguration;
+import org.jredis.server.JRedisServer;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.io.IOException;
+
+public class JRedisServerTest {
+
+  @Test
+  public void testServer() throws IOException {
+    JRedisServer server = new JRedisServer(new JRedisConfiguration());
+    ClassLayout layout = ClassLayout.parseInstance(server);
+    System.out.println(layout.toPrintable());
+
+    layout = ClassLayout.parseClass(RedisDatabase.class);
+    System.out.println(layout.toPrintable());
+
+    layout = ClassLayout.parseClass(JRIncrementalHash.class);
+    System.out.println(layout.toPrintable());
+
+    server.shutDown(0);
+  }
+
+  public static void main(String[] args) throws IOException {
+    JRedisServer server = new JRedisServer(new JRedisConfiguration());
+    ClassLayout layout = ClassLayout.parseInstance(server);
+    System.out.println(layout.toPrintable());
+
+    layout = ClassLayout.parseClass(RedisDatabase.class);
+    System.out.println(layout.toPrintable());
+
+    layout = ClassLayout.parseClass(JRIncrementalHash.class);
+    System.out.println(layout.toPrintable());
+
+    server.shutDown(0);
+  }
+}
